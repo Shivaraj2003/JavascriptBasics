a43c3938923d43e514ac66fa1b169a14251937e1
Shivaraj2003 <shivarajshetty635@gmail.com>
2024-01-05 12:13:07 +0530

    Functions in JS

---
+++ Functions/funtion.js
@@ -0,0 +1,45 @@
+
+function happy()
+{
+    console.log('I am good');
+}
+happy()
+
+function add(a,b)
+{
+    return a+b;
+}
+console.log(add(10,10));
+
+//Function expression
+
+var msg = function()
+            {
+                console.log('hii');
+            }
+msg();
+
+//IIFE
+(function(a,b)
+{
+    console.log("The answer is "+(a+b));
+})(10,2)
+
+//call
+let animal={
+    name:'dog',
+    eat(a,b){
+        console.log(this.name+' is eating '+a+' '+b);
+    }
+};
+let human= {
+    name:'ravi',
+    
+}
+animal.eat(5,'bones');
+animal.eat.call(human,2,'chips');
+
+let human_eat = animal.eat.bind(human);
+human_eat(3,'apples')
+
+
